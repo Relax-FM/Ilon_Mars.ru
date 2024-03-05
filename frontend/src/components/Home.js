66a6de004dd81d7b70276d633c832c3773acf3ed
nomilious <eduard.belic.2002@gmail.com>
2024-03-06 00:30:07 +0300

    Implemented basic logic of main Screen

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,13 +2,16 @@ import {Link} from "react-router-dom";
 import {useState} from "react";
 
 function Home() {
-    const [mode, setMode] = useState("earth");
+    const [mode, setMode] = useState("mars");
+    const [text, setText] = useState("Земля");
 
     const toggleMode = () => {
         if (mode === "earth") {
             setMode("mars");
+            setText("Земля");
         } else {
             setMode("earth");
+            setText("Марс");
         }
     };
 
@@ -16,19 +19,19 @@ function Home() {
         <div className={`page ${mode}`}>
             <div className="page__header">
                 <h1 className="page__title">
-                    {mode === "earth" ? "Земля. " : "Марс. "}
+                    {text + ". "}
                     Наука. Будущее.
                 </h1>
             </div>
             <div className="page__body info">
-                <button className="no-button info__item-earth" onClick={toggleMode}>
+                <button className="no-button info__item-planet" onClick={toggleMode} data-planet={text}>
                 </button>
                 <Link to="/auth">
                     <button className="no-button info__item-btn">
                         Авторизация
                     </button>
                 </Link>
-                <button className="no-button info__item-time"></button>
+                <button className="no-button info__item-time" data-time="1:00(Msc)"></button>
             </div>
 
         </div>
