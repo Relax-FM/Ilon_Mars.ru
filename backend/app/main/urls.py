cb4fd811e9d1b84e28fb4319d4092290d60863ad
Relax_FM <71275009+Relax-FM@users.noreply.github.com>
2024-03-09 01:15:56 +0300

    ADD: auth api

--- backend/app/main/urls.py
+++ backend/app/main/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import include
-
+from backend.app.auth.views import *
 
 def basic(request):
     return "Ilon Mars"
@@ -10,4 +10,7 @@ def basic(request):
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('/', basic),
+    re_path('api/login/', mylogin),
+    re_path('api/signup/', mysignup),
+    re_path('api/token/', test_token),
 ]
