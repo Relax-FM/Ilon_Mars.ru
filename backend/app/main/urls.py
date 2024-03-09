999b06fb3e5f8ea35f063aba46420b8b9fd5e4ee
Relax_FM <71275009+Relax-FM@users.noreply.github.com>
2024-03-10 01:14:02 +0300

    TEST: test send func

--- backend/app/main/urls.py
+++ backend/app/main/urls.py
@@ -3,14 +3,11 @@ from django.urls import path, re_path
 from django.conf.urls import include
 from auth.views import *
 
-def basic(request):
-    return "Ilon Mars"
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('/', basic),
+    path('test/send/', test_send),
     re_path('api/login/', mylogin),
     re_path('api/signup/', mysignup),
-    re_path('api/token/', test_token)
+    re_path('api/token/', test_token),
 ]
