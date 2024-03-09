bc09247b6925fbce51d733f43ac1d3e4ee3d6f55
Relax_FM <71275009+Relax-FM@users.noreply.github.com>
2024-03-10 02:39:26 +0300

    FIX: fix urls and views files of auth api

--- backend/app/auth/views.py
+++ backend/app/auth/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import logout, login
-from serializers import UserSerializer
-# from .serializers import UserSerializer
+# from serializers import UserSerializer
+from .serializers import UserSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
