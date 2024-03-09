65d144a7989bef40e1df15c7491fc188a78a98c5
Relax_FM <71275009+Relax-FM@users.noreply.github.com>
2024-03-09 16:58:47 +0300

    FIX: fix imports

--- backend/app/main/urls.py
+++ backend/app/main/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, re_path
 from django.conf.urls import include
-from backend.app.auth.views import *
+from auth.views import *
 
 def basic(request):
     return "Ilon Mars"
