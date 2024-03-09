2d64d9f126292baf5de963dad32578488d3ce878
Relax_FM <71275009+Relax-FM@users.noreply.github.com>
2024-03-10 01:43:21 +0300

    FIX: schedule multiple send

--- backend/app/schedule/views.py
+++ backend/app/schedule/views.py
@@ -14,8 +14,16 @@ def schedules(request):
         return Response(serializer.data)
 
     elif request.method == 'POST':
-        serializer = ScheduleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        requests = request.data
+        for req in requests:
+            req['start_time'] = req.pop('from')
+            req['end_time'] = req.pop('to')
+
+        for req in requests:
+            serializer = ScheduleSerializer(data=req)
+            if serializer.is_valid():
+                serializer.save()
+            else:
+                print('Bad request')
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
