379424f05fe0e56ad18ecd33f001133be5b4e8ed
Relax_FM <71275009+Relax-FM@users.noreply.github.com>
2024-03-10 15:44:04 +0300

    FIX: send new scientist to mars

--- backend/app/authapi/urls.py
+++ backend/app/authapi/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('test/send/', test_send),
     path('api/login/', mylogin),
     path('api/send_scientist/', mysignup),
     path('api/token/', test_token),
