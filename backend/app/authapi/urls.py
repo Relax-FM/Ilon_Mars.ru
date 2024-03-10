7f0e21a60918f5ff001d6b9b493f4d82baa56f08
u.dukuev <u.dukuev@ase-ec.ru>
2024-03-10 16:24:47 +0300

    1

--- backend/app/authapi/urls.py
+++ backend/app/authapi/urls.py
@@ -5,8 +5,6 @@ from .views import *
 urlpatterns = [
 <<<<<<< HEAD
     path('test/send/', test_send),
-=======
->>>>>>> 44ac8c6d76c312735fa7ebfbbb0255dbdf519a97
     path('api/login/', mylogin),
     path('api/get_scientist/', get_scientist),
     path('api/token/', test_token),
--- backend/app/main/urls.py
+++ backend/app/main/urls.py
@@ -19,18 +19,13 @@ from auth.views import mylogin, test_token, mysignup
 
 def basic(request):
     return "Ilon Mars"
->>>>>>> 44ac8c6d76c312735fa7ebfbbb0255dbdf519a97
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authapi.urls')),
-<<<<<<< HEAD
     path('', include('authapi.urls')),
-=======
->>>>>>> 44ac8c6d76c312735fa7ebfbbb0255dbdf519a97
     path('', include('schedule.urls')),
-    path('', basic),
     #path('/', basic),
     re_path('api/login/', mylogin),
     re_path('api/signup/', mysignup),
